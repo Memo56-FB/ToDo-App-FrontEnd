07235bf190e6185b26141bbd5ed09aa04b8fc741
Guillermo Farfan <memo56_fb@hotmail.com>
2022-04-02 16:48:30 -0600

    👩👨 feat: add login

--- src/pages/LoginRegister/components/Login/Login.jsx
+++ src/pages/LoginRegister/components/Login/Login.jsx
@@ -1,18 +1,43 @@
+import { useForm } from 'react-hook-form'
+
 const Login = () => {
+  const { register, handleSubmit } = useForm()
+
+  const fetchLogin = async (data) => {
+    const API_URL = `${import.meta.env.VITE_API_URL}/api/login`
+    const settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    try {
+      const fetchResponse = await fetch(API_URL, settings)
+      console.log(await fetchResponse.json())
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <form className='login-register__form'>
+    <form className='login-register__form' onSubmit={handleSubmit(fetchLogin)}>
       <label htmlFor='username'>
         Username:
-        <input type='text' name='username' autoComplete='username' />
+        <input {...register('username')} autoComplete='username' />
       </label>
       <label htmlFor='password'>
         Password:
         <input
           type='password'
-          name='password'
+          {...register('password')}
           autoComplete='new-password'
         />
       </label>
+      <button type="submit" className='button'>
+        Enviar
+      </button>
     </form>
   )
 }
