ab6101ddeb662093b9826c6976bdb4dbedca2d33
Guillermo Farfan <memo56_fb@hotmail.com>
2022-04-02 16:40:09 -0600

    👩👨 feat: add register

--- src/pages/LoginRegister/components/Register/Register.jsx
+++ src/pages/LoginRegister/components/Register/Register.jsx
@@ -1,22 +1,47 @@
+import { useForm } from 'react-hook-form'
+
 const Register = () => {
+  const { register, handleSubmit } = useForm()
+
+  const fetchRegister = async (data) => {
+    const API_URL = `${import.meta.env.VITE_API_URL}/api/users`
+    const settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+
+    try {
+      const fetchResponse = await fetch(API_URL, settings)
+      console.log(await fetchResponse.json())
+    } catch (err) {
+      return err
+    }
+  }
   return (
-    <form className='login-register__form'>
+    <form className='login-register__form' onSubmit={handleSubmit(fetchRegister)}>
       <label htmlFor='name'>
         Name:
-        <input type='text' name='name' autoComplete='name' />
+        <input {...register('name')} autoComplete='name' />
       </label>
       <label htmlFor='username'>
         Username:
-        <input type='text' name='username' autoComplete='username' />
+        <input {...register('username')} autoComplete='username' />
       </label>
       <label htmlFor='password'>
         Password:
         <input
           type='password'
-          name='password'
+          {...register('password')}
           autoComplete='new-password'
         />
       </label>
+      <button type="submit" className='button'>
+        Enviar
+      </button>
     </form>
   )
 }
