3cd64464a3234b9ac60b97a3369f67de4f84f616
Guillermo Farfan <memo56_fb@hotmail.com>
2022-04-02 16:02:59 -0600

    📏 style: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/jsx-indent': ['error', 'spaces' | 2, { checkAttributes: true, indentLogicalExpressions: true }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
+  }
+}
--- src/pages/LoginRegister/LoginRegister.jsx
+++ src/pages/LoginRegister/LoginRegister.jsx
@@ -1,38 +1,39 @@
-import "./LoginRegister.scss"
-import { Header } from "../../components/Header/Header"
-import { NavLink, useLocation } from "react-router-dom"
-import { Register } from './components/Register/Register'
+import './LoginRegister.scss'
+import { NavLink, useLocation } from 'react-router-dom'
+
+import { Header } from '../../components/Header/Header'
 import { Login } from './components/Login/Login'
+import { Register } from './components/Register/Register'
 const LoginRegister = () => {
   const location = useLocation()
 
-	let activeStyle = {
-		color: "hsl(192, 100%, 67%)"
-	}
-	const onActive = ({ isActive }) => (isActive ? activeStyle : undefined)
+  const activeStyle = {
+    color: 'hsl(192, 100%, 67%)'
+  }
+  const onActive = ({ isActive }) => (isActive ? activeStyle : undefined)
 
-	return (
-		<>
-			<Header />
-			<main className='padding-app flex flex-col w-full  absolute -mt-20 md:-mt-12 lg:-mt-24 2xl:-mt-44'>
-				<div className='flex justify-between text-white text-2xl mb-4'>
-					<NavLink to={"/register"} style={onActive} >
-						Register
-					</NavLink>
-					<NavLink to={"/login"} style={onActive} >Login</NavLink>
-				</div>
-				<section className='bg-Very-Dark-Desaturated-Blue text-lg p-4 rounded-md'>
+  return (
+    <>
+      <Header />
+      <main className='padding-app flex flex-col w-full  absolute -mt-20 md:-mt-12 lg:-mt-24 2xl:-mt-44'>
+        <div className='flex justify-between text-white text-2xl mb-4'>
+          <NavLink to={'/register'} style={onActive} >
+            Register
+          </NavLink>
+          <NavLink to={'/login'} style={onActive} >Login</NavLink>
+        </div>
+        <section className='bg-Very-Dark-Desaturated-Blue text-lg p-4 rounded-md'>
           {
             location.pathname === '/register' && <Register />
           }
           {
             location.pathname === '/login' && <Login />
           }
-					
-				</section>
-			</main>
-		</>
-	)
+
+        </section>
+      </main>
+    </>
+  )
 }
 
 export { LoginRegister }
