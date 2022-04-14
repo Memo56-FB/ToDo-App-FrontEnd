5c18f0a22a831aeed3fd3a1e678835a8c4c9e024
macbook <macbook@macbooks-MacBook-Pro.local>
2022-04-14 15:45:00 -0500

    🐛 fix: toggle complete todo

--- src/pages/TodoList/components/ToggleTodoComplete/ToggleTodoComplete.jsx
+++ src/pages/TodoList/components/ToggleTodoComplete/ToggleTodoComplete.jsx
@@ -22,9 +22,18 @@ const ToggleTodoComplete = ({ todo, token }) => {
 
     try {
       // TODO: agregar validaciones en caso de 200 o 400
-      await fetch(API_URL, settings)
-      setLoading(false)
+      const fetchResponse = await fetch(API_URL, settings)
+      if (fetchResponse.status === 200) {
+        todo.complete = !todoChecked
+        setLoading(false)
+      }
+      if (fetchResponse.status === 400) {
+        window.alert('Ups, algo salio mal, recarga la página e intentalo de nuevo')
+        setLoading(false)
+      }
     } catch (err) {
+      window.alert('Ups, algo salio mal, recarga la página e intentalo de nuevo')
+      setLoading(false)
       console.error(err)
     }
   }
@@ -37,9 +46,8 @@ const ToggleTodoComplete = ({ todo, token }) => {
       type='button'
       disabled={loading}
       onClick={toggleCheckedTodo}
-      className={`rounded-full cursor-pointer ${
-        todoChecked ? 'todo-checked' : 'todo-unchecked'
-      }`}
+      className={`rounded-full cursor-pointer ${todoChecked ? 'todo-checked' : 'todo-unchecked'
+        }`}
     >
       {todoChecked && (
         <svg xmlns='http://www.w3.org/2000/svg' width='11' height='9'>
